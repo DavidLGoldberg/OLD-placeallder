301c61f8adf413de2491bc1eca4c991002afe630
David Goldberg <DavidLGoldberg@gmail.com>
2012-02-20 14:44:21 -0500

    adds a placewholeder class.  adds a bit of test styling.  adds incrementing overlays.  Intercepts all inputs with placeholder text set.

--- placewholeder.js
+++ placewholeder.js
@@ -1,3 +1,12 @@
 $(function(){
-    alert('hi');
+    var $inputs = $('input');
+    var count = 0; //There's a way to do this with the $.each I beleive...
+    $inputs.each(function(){
+        count++;
+        var placeholderText = $(this).attr('placeholder');
+        if (placeholderText){
+            $(this).parent().append('<div id="placewholeder-overlay-' + count + '"'
+                + 'class="placewholeder">' + placeholderText + '</div>');
+        }
+    });
 });
