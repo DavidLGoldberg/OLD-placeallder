c2676ff7017060e02a788ca92fd6fa722014a93c
David Goldberg <DavidLGoldberg@gmail.com>
2012-02-20 15:44:32 -0500

    mostly both disappearing and reappearing with focuses and blurs.

--- placewholeder.js
+++ placewholeder.js
@@ -1,27 +1,38 @@
-$(function(){
-    var $inputs = $('input');
-    var count = 0; //There's a way to do this with the $.each I beleive...
-    $inputs.each(function(){
-        count++;
-        var $input = $(this);
+(function(){
+    var overlayCount = 0;
+
+    var initOverlay = function($input) {
+        overlayCount++;
         var placeholderText = $input.attr('placeholder');
-        if (placeholderText){
-            $input.parent().append(
-                $('<div id="placewholeder-overlay-' + count + '"'
-                    + 'class="placewholeder"'
-                    + 'style="position: absolute;'
-                        + 'z-index: 2;'
-                        + 'left: ' + $input.position().left + ';'
-                        + 'top: ' + $input.position().top + ';'
-                        + '">'
-                    + placeholderText +
-                '</div>')
-                    .click(function(){
-                        var $placewholeder = $(this);
-                        $placewholeder.css('display',  'none'); 
-                        $input.focus();
-                    })
-            );
-        }
+        $input.parent().append(
+            $('<div id="placewholeder-overlay-' + overlayCount + '"'
+                + 'class="placewholeder"'
+                + 'style="position: absolute;'
+                    + 'z-index: 2;'
+                    + 'left: ' + $input.position().left + ';'
+                    + 'top: ' + $input.position().top + ';'
+                    + '">'
+                + placeholderText +
+            '</div>')
+                .click(function(){
+                    var $placewholeder = $(this);
+                    $placewholeder.css('display',  'none');  //change this to remove?
+                    $input.focus();
+                })
+        );   
+    };
+
+    $(function(){
+        var $inputs = $('input');
+        $inputs.each(function(){
+            var $input = $(this);
+            if ($input.attr('placeholder')){
+                initOverlay($input);
+
+                $input.blur(function() {
+                    initOverlay($input);
+                });
+            }
+        });
     });
-});
+})();
