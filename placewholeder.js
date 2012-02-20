cfd8ec31d14ee3b7f60c0c752c86bfb5ba2d3e15
David Goldberg <DavidLGoldberg@gmail.com>
2012-02-20 15:20:54 -0500

    initial placement of gray overlay text.

--- placewholeder.js
+++ placewholeder.js
@@ -3,13 +3,25 @@ $(function(){
     var count = 0; //There's a way to do this with the $.each I beleive...
     $inputs.each(function(){
         count++;
-        var placeholderText = $(this).attr('placeholder');
+        var $input = $(this);
+        var placeholderText = $input.attr('placeholder');
         if (placeholderText){
-            $(this).parent().append(
+            $input.parent().append(
                 '<div id="placewholeder-overlay-' + count + '"'
-                    + 'class="placewholeder">'
+                    + 'class="placewholeder"'
+                    + 'style="position: absolute;'
+                        + 'z-index: 2;'
+                        + 'left: ' + $input.position().left + ';'
+                        + 'top: ' + $input.position().top + ';'
+                        + '">'
                     + placeholderText +
                 '</div>');
+                    //.click(function(){
+                        //debugger;
+                        //var $placewholeder = $(this);
+                        //$placewholeder.css('display: none;'); 
+                        //$input.blur();
+                    //});
         }
     });
 });
